a4c3a8ebb3f9edd4429d19b2e340e42a8c96dc9b
Rick Meijer <info@rickmeijer.net>
2023-03-07 13:18:27 +0100

    feat: add looper

--- cli/create/svelte/index.ts
+++ cli/create/svelte/index.ts
@@ -6,14 +6,14 @@ import { create as createSvelte } from "create-svelte";
 export async function create(projectName: string) {
   if (!projectName)
     return log.error("Please run the command with a project name argument");
-  console.log(bold("Creating a new project in apps/"), projectName);
-  // await createSvelte(`apps/${projectName}`, {
-  //   name: projectName,
-  //   template: "skeleton", // or 'skeleton' or 'skeletonlib'
-  //   types: "typescript", // or 'typescript' or null;
-  //   prettier: true,
-  //   eslint: true,
-  //   playwright: true,
-  //   vitest: false,
-  // });
+  log.info(`Creating a new svelte project in apps/${projectName}`);
+  await createSvelte(`apps/${projectName}`, {
+    name: projectName,
+    template: "skeleton", // or 'skeleton' or 'skeletonlib'
+    types: "typescript", // or 'typescript' or null;
+    prettier: true,
+    eslint: true,
+    playwright: true,
+    vitest: false,
+  });
 }
