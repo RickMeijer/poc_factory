64dfebae1d5a53dc11d761b6d5956b125c925467
Rick Meijer <info@rickmeijer.net>
2023-03-07 12:13:23 +0100

    feat: add inquirer

--- index.ts
+++
@@ -1,5 +0,0 @@
-async function main() {
-  const fooModule = await import("./foo.js");
-  console.dir({ foo: fooModule });
-}
-main();
--- start.ts
+++ start.ts
@@ -1,3 +1,58 @@
-(async function create(projectName = process.argv[2]) {
-  (await import(`./cli/create/svelte/index.js`)).create(projectName);
-})();
+import inquirer from "inquirer";
+
+const prompt = inquirer.createPromptModule();
+(async function init(scope, defaultAction) {
+  const answer = await prompt([
+    {
+      type: "list",
+      name: "init",
+      message: "What do you want to do?",
+      default: defaultAction,
+      choices: [
+        { name: "Create a new project", value: "create" },
+        // { name: "Start a project", value: "start" },
+        // { name: "Test the codebase", value: "test" },
+        { name: "Update installed packages", value: "update" },
+        // { name: "Trigger bitbucket pipeline", value: "pipelinetrigger" },
+        // { name: "Other", value: "other" },
+      ],
+    },
+  ]);
+  const action: keyof typeof scope = answer.init;
+  return await scope[action](answer);
+})(
+  {
+    start,
+    create,
+  },
+  // allows for `npm start create`, for example
+  process.argv[2] || "start"
+);
+
+function start(answers) {
+  return prompt([
+    {
+      type: "list",
+      name: "start",
+      choices: [{ name: "foo", value: "foo" }],
+    },
+  ]);
+}
+
+async function create() {
+  const { projectname, framework } = await prompt([
+    {
+      message: "What is the name of the project?",
+      type: "input",
+      name: "projectname",
+    },
+    {
+      message: "Framework of choice?",
+      type: "list",
+      name: "framework",
+      choices: ["svelte", "react", { name: "solidjs", disabled: true }],
+    },
+  ]);
+
+  (await import(`./cli/create/${framework}/index.js`)).create(projectname);
+}
